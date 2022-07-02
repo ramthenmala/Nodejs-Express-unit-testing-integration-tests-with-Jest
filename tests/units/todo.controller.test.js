f64eb7420d8038f1ffdd1a689ade2c68c09dd3ec
Ram kumar <webbieram@outlook.com>
2022-07-03 01:21:40 +0530

    Test if response is sending back JSON body

--- tests/units/todo.controller.test.js
+++ tests/units/todo.controller.test.js
@@ -4,27 +4,33 @@ const httpMocks = require('node-mocks-http');
 const todoData = require('../mockData/todo/todo.json')
 
 TodoModel.create = jest.fn();
-
+let res, req, next;
+// Runs before every test
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 describe('TodoController', function () {
-  let res, req, next;
-  // Runs before every test
   beforeEach(() => {
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null;
-  });
+    req.body = todoData;
+  })
   it('Should Have CreateTodoFunction', () => {
     expect(typeof TodoController.createTodo).toBe('function');
   });
   it('Should call todo method', () => {
-    req.body = todoData;
+    
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(todoData);
   });
   it('Should respond with status code 201', ()=> {
-      req.body = todoData;
       TodoController.createTodo(req, res, next);
       expect(res.statusCode).toBe(201);
       expect(res._isEndCalled()).toBeTruthy();
+  })
+  it('Should return a json body', ()=> {
+      TodoModel.create.mockReturnValue(todoData);
+      TodoController.createTodo(req, res, next);
+      expect(res._getJSONData()).toStrictEqual(todoData);
   })
 })
