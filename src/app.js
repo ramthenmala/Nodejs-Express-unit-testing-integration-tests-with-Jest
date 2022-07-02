32b3131551208fdc67071e6a557ddecc1d822ddd
Ram kumar <webbieram@outlook.com>
2022-07-02 20:55:30 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+      msg: 'Root Log',
+    });
+})
+
+app.listen(3000, ()=> {
+    console.log(`Listening on port 3000`)
+})
